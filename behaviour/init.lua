4eeb0e7aa84878f88f7fb63d10a4a83a0b86603b
Fernando Paredes García <fernando@develcuy.com>
2012-08-08 16:30:21 -0500

    Add function temp_dir(), from Nutria Nemo.

--- behaviour/init.lua
+++ behaviour/init.lua
@@ -1,6 +1,8 @@
 require [[seawolf.fs]]
 local is_file, loadfile = seawolf.fs.is_file, loadfile
-module([[seawolf.behaviour]])
+local getenv = os.getenv
+
+module [[seawolf.behaviour]]
 
 local config
 
@@ -23,3 +25,14 @@ function ini_get(param)
   end
 end
 
+-- Returns directory path used for temporary files
+do
+  local path
+  function temp_dir()
+    if path == nil then
+      -- TODO: Test in Windows and Mac
+      path = getenv [[TMP]] or getenv [[TEMP]] or getenv [[TMPDIR]]  or [[/tmp]]
+    end
+    return path
+  end
+end
