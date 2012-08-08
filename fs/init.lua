f00eada08dcad4a30ab1ae0dcb8a47d03406270f
Fernando Paredes García <fernando@develcuy.com>
2012-08-08 16:31:03 -0500

    Implement functions: safe_open(), safe_write() and safe_close().

--- fs/init.lua
+++ fs/init.lua
@@ -1,4 +1,7 @@
-local posix, lfs, string, package = require [[posix]], require [[lfs]], string, package
+local posix, lfs, string = require [[posix]], require [[lfs]], string
+local package, socket = package = require [[socket]]
+local rename, remove, open = os.rename, os.remove, io.open
+
 module [[seawolf.fs]]
 
 -- LUA_DIRSEP is the first character in the string package.config, see
@@ -58,4 +61,119 @@ end
 -- TODO: validate "length"
 function fread(handle, length)
   return handle:read(length)
+end
+
+-- Thread-safe open file in read mode
+function safe_open(filepath, timeout, retry, sign)
+  if not timeout then timeout = 0.001 end
+  if not retry then retry = 0 end -- for internal use only!
+  if not sign then sign = [[]] end -- for internal use only!
+
+  local fh, err
+  local file_lock, file_sign = filepath .. [[.lock]]
+
+  -- Try to open lock file
+  fh, err = io.open(file_lock)
+  if fh then
+    -- Validate signature
+    file_sign = fh:read [[*l]]
+    fh:close()
+    if file_sign == sign and sign:len() > 0 then
+      -- Pre-create target file
+      fh, err = io.open(filepath, [[a+]])
+      if fh then
+        fh:close()
+        fh, err = io.open(filepath)
+        return fh, sign, err
+      else
+        error [[I/O error: can't create session.]]
+      end
+    else
+      -- Other process has set a read lock, wait a bit and retry
+      sleep(timeout)
+      retry = retry + 1
+      if retry <= 224 then
+        return safe_open(filepath, timeout, retry)
+      else
+        return nil, ([[%s retries without luck :(]]):format(retry - 1)
+      end
+    end
+  else
+    -- Try to lock
+    fh, err = io.open(file_lock, [[a+]])
+    if fh then
+      -- Check is not signed yet
+      if not fh:read [[*l]] then
+        file_sign = uuid.new()
+        fh:write(file_sign)
+        fh:write("\n")
+        fh:close()
+        -- Try to re-open lock
+        return safe_open(filepath, timeout, retry, file_sign)
+      else        
+        sleep(timeout)
+        retry = retry + 1
+        if retry <= 224 then
+          return safe_open(filepath, timeout, retry)
+        else
+          return nil, ([[%s retries without luck :(]]):format(retry - 1)
+        end
+      end
+    else
+      return nil, [[Can't lock: retry later]]
+    end
+  end
+end
+
+-- Write to file opened by safe_open()
+function safe_write(filepath, sign, data)
+  if not sign then sign = [[]] end 
+
+  local fh, err, unlocked
+  local file_lock, file_sign = filepath .. [[.lock]]
+
+  -- Try to open lock file
+  fh, err = io.open(file_lock)
+  if fh then
+    -- Validate signature
+    file_sign = fh:read [[*l]]
+    fh:close()
+    if file_sign == sign and sign:len() > 0 then
+      -- Try to open target file
+      fh, err = io.open(filepath, [[w]])
+      if fh then
+        -- Save data
+        fh:write(data)
+        fh:close()
+        return true
+      else
+        return nil, [[Can't open target file]]
+      end
+    else
+      return nil,  [[Can't write: invalid sign]]
+    end
+  else
+    return nil, [[Can't open lock]]
+  end
+end
+
+-- Unlock file opened by safe_open()
+function safe_close(filepath, sign)
+  local file_lock, file_sign = filepath .. [[.lock]]
+
+  fh, err = io.open(file_lock)
+  if fh then
+    -- Validate signature
+    file_sign = fh:read [[*l]]
+    fh:close()
+    if file_sign == sign and sign:len() > 0 then
+      -- Unlock
+      unlocked, err = remove(file_lock)
+      if unlocked then
+        return unlocked
+      else
+        return nil, ([[Can't remove read lock (%s)]]):format(err)
+      end
+    end
+  end
 end
