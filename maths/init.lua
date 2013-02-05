02714cb394ba27391d2be7ec7fe992bd3f7a47fa
Fernando Paredes Garcia <fernando@develcuy.com>
2013-02-05 14:20:07 -0500

    Implement function round().

--- maths/init.lua
+++ maths/init.lua
@@ -1,10 +1,13 @@
-require [[random]]
+local random, floor, ceil = require [[random]], math.floor, math.ceil
+local time = os.time
 
-MT_RAND_GENERATOR = random.new(os.time())
+module [[seawolf.maths]]
+
+MT_RAND_GENERATOR = random.new(time())
 
 -- Generate a better random value
 function mt_rand()
-  return math.floor(MT_RAND_GENERATOR() * mt_getrandmax())
+  return floor(MT_RAND_GENERATOR() * mt_getrandmax())
 end
 
 -- Show largest possible random value
@@ -17,4 +20,13 @@ function mt_getrandmax()
          -- 2*2*2*2*2*
          -- 2 - 1
   return 2147483647 -- Same as the PHP hardcoded one in ext/standard/php_rand.h
+end
+
+
+-- Rounds a float
+-- Copied from http://lua-users.org/wiki/SimpleRound
+function round(num, idp)
+  local mult = 10^(idp or 0)
+  if num >= 0 then return floor(num * mult + 0.5) / mult
+  else return ceil(num * mult - 0.5) / mult end
 end
